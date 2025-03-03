63ea2ce42f102a124a71cf0c13303b28eea125dd
wassim-oucouc <wassimoucouc03@gmail.com>
2025-03-03 08:57:01 +0000

    Update LoginRequest.php

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -13,21 +13,21 @@ class LoginRequest extends FormRequest
 {
     public function authorize(): bool
     {
-        return true; // Autorise l'utilisateur à soumettre la demande.
+        return true; 
     }
 
     public function rules(): array
     {
         return [
-            'email' => ['required', 'string', 'email'], // Valide que l'email est bien formé.
-            'password' => ['required', 'string'], // Valide que le mot de passe est fourni.
+            'email' => ['required', 'string', 'email'], 
+            'password' => ['required', 'string'], 
         ];
     }
     public function authenticate(): void
     {
-        $credentials = $this->only('email', 'password'); // 'email' est en minuscule ici
+        $credentials = $this->only('email', 'password');
     
-        // Si votre colonne est en minuscule, utilisez simplement 'email' sans modification
+    
         if (! Auth::attempt($credentials, $this->boolean('remember'))) {
             RateLimiter::hit($this->throttleKey());
     
@@ -43,7 +43,6 @@ class LoginRequest extends FormRequest
 
     public function ensureIsNotRateLimited(): void
     {
-        // Si trop de tentatives ont échoué, bloque l'utilisateur pendant un certain temps.
         if (RateLimiter::tooManyAttempts($this->throttleKey(), 5)) {
             event(new Lockout($this));
 
@@ -60,7 +59,6 @@ class LoginRequest extends FormRequest
 
     public function throttleKey(): string
     {
-        // Crée une clé unique pour limiter les tentatives basées sur l'email et l'adresse IP
         return Str::lower($this->input('email')) . '|' . $this->ip();
     }
 }
